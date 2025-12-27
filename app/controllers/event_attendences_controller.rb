3fcb0f3785bedebb13d8d52ae6e616ce6ffa8dbb
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-12-27 15:28:49 +0000

    feat: add even attendence controller

---
+++ app/controllers/event_attendences_controller.rb
@@ -0,0 +1,33 @@
+class EventAttendencesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event
+
+  def create
+    attendence = EventAttendence.new(
+      attendee: current_user,
+      attended_event: @event
+    )
+
+    if attendence.save
+      redirect_to @event, notice: "You are now attending this event"
+    else
+      redirect_to @event, notice: "You are already attending this event"
+    end
+  end
+
+  def destroy
+    attendence = EventAttendence.find_by(
+      attendee: current_user,
+      attended_event: @event
+    )
+
+    attendence&.destroy
+    redirect_to @event, notice: "You are no longer attending this event."
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+end
