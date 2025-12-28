d4c55ab1b36e78200002fbba22cb98386e7a29c4
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-12-28 14:44:33 +0000

    chore: refactor EventInvitations controller

--- app/controllers/event_invitations_controller.rb
+++ app/controllers/event_invitations_controller.rb
@@ -1,17 +1,27 @@
 class EventInvitationsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_event
+  before_action :set_invitee
 
   def create
     return head :forbidden unless @event.creator == current_user
 
-    @event.event_invitations.create(invitee_id: params[:invitee_id])
-    redirect_to @event, notice: "User invited"
+    invite = @event.event_invitations.build(invitee: @invitee)
+
+    if invite.save
+      redirect_to @event, notice: "User Invited"
+    else
+      redirect_to @event, alert: "User has already been invited."
+    end
   end
 
   private
 
   def set_event
     @event = Event.find(params[:event_id])
+  end
+
+  def set_invitee
+    @invitee = User.find(params[:invitee_id])
   end
 end
