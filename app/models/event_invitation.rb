45d52465e5a65860b6261fd999616a2113854294
fredymarb <29468132+fredymarb@users.noreply.github.com>
2025-12-28 14:48:41 +0000

    add validation to prevent duplicate records in the EventInvitations model

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -1,4 +1,6 @@
 class EventInvitation < ApplicationRecord
   belongs_to :event, class_name: "Event"
   belongs_to :invitee, class_name: "User"
+
+  validates :invitee_id, uniqueness: { scope: :event_id }
 end
